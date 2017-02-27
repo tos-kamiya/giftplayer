579a2618b3232211b60612f9657098873b58bc15
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2017-02-27 20:31:31 +0900

    fix and simplify command line

--- giftplay_run.py
+++ giftplay_run.py
@@ -6,30 +6,20 @@ from giftplay import html_form
 from giftplay import web_player
 
 
-@click.group(invoke_without_command=True)
-@click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
-        print(ctx.get_help())
-
-
-@cli.command(help='Generate HTML')
+@click.command(help="Render given GIFT script as HTML.")
 @click.argument('gift_script', default='')
 @click.option('--shuffle', '-s', help='Seed of shuffling choices of each question', default=-1)
-@click.option('--debug-wo-hint', help='Debug option. Generate HTML w/o parsing answer', default=False)
-def html(gift_script, shuffle, debug_wo_hint):
-    html_form.entrypoint(gift_script, False, shuffle, debug_wo_hint)
-
-
-@cli.command(help='Run as web app')
-@click.argument('gift_script', default='')
-@click.option('--shuffle', '-s', help='Seed of shuffling choices of each question', default=-1)
-def web(gift_script, shuffle):
-    if not sys.stdout.isatty():
-        sys.stdout.write("""<!DOCTYPE HTML><html><body>Go to <a href="http://127.0.0.1:5000">giftplay server</a></body></html>\n""")
-        sys.stdout.flush()
-    web_player.entrypoint(gift_script, shuffle)
+@click.option('--web-server', '-w', help='Run as web app', is_flag=True)
+@click.option('--debug-wo-hint', help='Debug option. Generate HTML w/o parsing answer', is_flag=True)
+def cmd(gift_script, shuffle, web_server, debug_wo_hint):
+    if web_server:
+        if not sys.stdout.isatty():
+            sys.stdout.write("""<!DOCTYPE HTML><html><body>Go to <a href="http://127.0.0.1:5000">giftplay server</a></body></html>\n""")
+            sys.stdout.flush()
+        web_player.entrypoint(gift_script, shuffle)
+    else:
+        html_form.entrypoint(gift_script, False, shuffle, debug_wo_hint)
 
 
 if __name__ == '__main__':
-    cli()
+    cmd()
